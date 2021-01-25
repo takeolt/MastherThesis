6a24b8733d1da6f6e43fafd4dec0efcd0b06b1d0
takeolt <paulius476@gmail.com>
2021-01-25 16:53:05 +0100

    second update

--- MastherThesis/app/src/main/java/com/example/mastherthesis/MainActivity.java
+++ MastherThesis/app/src/main/java/com/example/mastherthesis/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.mastherthesis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_email, et_subject, et_message;
+
+    Button et_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_email = (EditText) findViewById(R.id.et_email);
+        et_subject = (EditText) findViewById(R.id.et_subject);
+        et_message = (EditText) findViewById(R.id.et_message);
+
+        et_send = (Button) findViewById(R.id.et_send);
+
+        et_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String to = et_email.getText().toString();
+                String subject = et_subject.getText().toString();
+                String message = et_message.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(intent.EXTRA_EMAIL, new String[] {to});
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                intent.putExtra(Intent.EXTRA_TEXT, message);
+
+                intent.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(intent, "Select Email app"));
+            }
+        });
     }
 }
