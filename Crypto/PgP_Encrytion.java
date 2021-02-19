f72c6e7b6a768ab6e18e85ae345308db7c07a7a1
takeolt <paulius476@gmail.com>
2021-02-19 13:25:05 +0100

    New crypto)

---
+++ Crypto/PgP_Encrytion.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.*;
+
+class PgP_Encryption {
+
+    public static void main(String[] args) {
+
+        BigInteger p = new BigInteger(1024, 100, new Random());
+        BigInteger q = new BigInteger(1024, 100, new Random());
+        
+
+    }
+
+}
