fe82fdbe868a1207c8f6f825d419e7c755262afe
takeolt <paulius476@gmail.com>
2021-03-05 20:39:38 +0100

    Update

--- MastherThesis/app/src/main/java/com/example/mastherthesis/Register.java
+++ MastherThesis/app/src/main/java/com/example/mastherthesis/Register.java
@@ -1,12 +1,21 @@
 package com.example.mastherthesis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
@@ -15,7 +24,7 @@ public class Register extends AppCompatActivity {
     TextView appName, goToLoginText, registerText;
     Button registerButton;
     FirebaseAuth firebaseAuth;
-
+    ProgressBar progress;
 
 
     @Override
@@ -31,8 +40,71 @@ public class Register extends AppCompatActivity {
         goToLoginText = findViewById(R.id.goToLoginText);
         registerText = findViewById(R.id.registerText);
         registerButton = findViewById(R.id.registerButton);
+        progress = findViewById(R.id.accountVerificationBar);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = emailAddress.getText().toString().trim();
+                String password1 = password.getText().toString().trim();
+                String password2 = rePassword.getText().toString().trim();
+                String personName = name.getText().toString().trim();
+
+
+                if(TextUtils.isEmpty(email)) {
+                    emailAddress.setError("Email is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(personName)) {
+                    name.setError("Name is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password1)) {
+                    password.setError("Password is required");
+                    return;
+                }
+
+                if(password1.length() < 6) {
+                    password.setError("Password is too short, password should be at least 6 of length");
+                    return;
+                }
+
+                if(!password1.equals(password2)) {
+                    rePassword.setError("Reentered password is not equal the password");
+                    return;
+                }
+
+                progress.setVisibility(View.VISIBLE);
+
+                firebaseAuth.createUserWithEmailAndPassword(email,password1).addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                }
+
+                                else {
+                                    Toast.makeText(Register.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        }
+                );
+            }
+        });
 
 
     }
