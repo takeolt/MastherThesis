29ace64e6777bccf29e96de7ab29ef4c11f89d22
Paulius Deveika <paulius476@gmail.com>
2021-08-23 05:29:44 +0100

    updatert look av appen

---
+++ Eemail/app/src/main/java/com/example/eemail/ui/SendEmails/SendEmail.java
@@ -0,0 +1,16 @@
+package com.example.eemail.ui.SendEmails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.eemail.R;
+
+public class SendEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_email);
+    }
+}
